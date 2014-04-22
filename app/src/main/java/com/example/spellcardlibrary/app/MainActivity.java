2e183952ee4eabc3176c19e0007bff19695812da
abantam <abantam.9@gmail.com>
2014-04-23 00:29:51 +0900

    作品名を選択するタブを実装（未完）

--- app/src/main/java/com/example/spellcardlibrary/app/MainActivity.java
+++ app/src/main/java/com/example/spellcardlibrary/app/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +13,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //タブホストのインスタンス化
+        TabHost host = (TabHost)findViewById(R.id.tabhost);
+        host.setup();
+
+        //タブに作品名を割り当てる
+        String[] workname = getResources().getStringArray(R.array.workname);
+        TabHost.TabSpec spec;
+        for(String wn : workname) {
+            spec = host.newTabSpec(wn);
+            spec.setContent(R.id.test);
+            spec.setIndicator(wn);
+            host.addTab(spec);
+        }
+
     }
 }

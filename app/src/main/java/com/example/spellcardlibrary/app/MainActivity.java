26d1f64e7f8d36c23b540de4097ef1f62cbf844c
abantam <abantam.9@gmail.com>
2014-05-02 22:40:51 +0900

    タブにインテントを実装した インテントのリストを作った

--- app/src/main/java/com/example/spellcardlibrary/app/MainActivity.java
+++ app/src/main/java/com/example/spellcardlibrary/app/MainActivity.java
@@ -9,6 +9,29 @@ import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
+    //各タブの起動するアクティビティを保持したクラス
+    Class classlist[] = {
+            Table_kouma.class,
+            Table_youmu.class,
+            Table_suimu.class,
+            Table_eiya.class,
+            Table_kaei.class,
+            Table_bunka.class,
+            Table_fuzin.class,
+            Table_hisou.class,
+            Table_tirei.class,
+            Table_seiren.class,
+            Table_tensoku.class,
+            Table_ds.class,
+            Table_daisen.class,
+            Table_sinrei.class,
+            Table_sinki.class,
+            Table_kisin.class,
+    };
+
+    //各タブに割り当てられたアクティビティを起動するためのインテント群
+    ArrayList<Intent> intentList = new ArrayList<Intent>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,16 +43,17 @@ public class MainActivity extends Activity {
 
         //タブに作品名を割り当てる
         String[] workname = getResources().getStringArray(R.array.workname);
-        String[] classlist = getResources().getStringArray(R.array.classlist);
         TabHost.TabSpec spec;
-        Intent i;
-        int j = 0;
-        for(String wn : workname) {
-            spec = host.newTabSpec(wn);
-            spec.setContent(new Intent(this, classlist[j].getClass()));
-            spec.setIndicator(wn);
+        for(int it = 0; it < classlist.length; it++) {
+
+            //インテントを作成
+            intentList.add(new Intent(this, classlist[it]));
+
+            //タブを作成しインテントを設定
+            spec = host.newTabSpec(workname[it]);
+            spec.setContent(intentList.get(it));
+            spec.setIndicator(workname[it]);
             host.addTab(spec);
-            j++;
         }
 
 

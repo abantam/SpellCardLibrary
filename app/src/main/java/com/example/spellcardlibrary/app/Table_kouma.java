f558dd48b85d4917b656245b91af4d8dd2799620
abantam <abantam.9@gmail.com>
2014-05-13 16:28:28 +0900

    紅魔郷のリストを表示させることに成功

--- app/src/main/java/com/example/spellcardlibrary/app/Table_kouma.java
+++ app/src/main/java/com/example/spellcardlibrary/app/Table_kouma.java
@@ -5,9 +5,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -17,15 +15,18 @@ import java.util.HashMap;
 
 public class Table_kouma extends Activity {
 
-    private DatabaseHelper mDbHelper;
+    private ListView mainView;
     private SQLiteDatabase db;
-    //private StationCodeDao mStationCodeDao;
+    private DatabaseHelper mDbHelper;
+    private ArrayList<HashMap<String, String>> scList = new ArrayList<HashMap<String, String>>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_kouma);
         setDatabase();
-        findData(1);
+        makeList();
+        setMainView();
     }
 
     @Override
@@ -46,10 +47,27 @@ public class Table_kouma extends Activity {
         }
     }
 
-    private static final String[] COLUMNS = {"*"};
+    private void makeList() {
 
-    private Cursor findData(int id) {
-        Cursor cursor = db.rawQuery("SELECT * FROM 東方紅魔郷 ", null);
-        return cursor;
+        //インテントを取得したあと作品名を取得
+        Intent intent = getIntent();
+        String workname = intent.getStringExtra("東方紅魔郷");
+
+        Cursor c = db.query(workname, null, null, null, null, null, null);
+        c.moveToFirst();
+
+        for(int i = 0; i < c.getCount(); i++, c.moveToNext()) {
+            HashMap<String, String> temp = new HashMap<String, String>();
+            temp.put("Number", c.getString(1));
+            temp.put("scName", c.getString(2));
+            scList.add(temp);
+        }
+    }
+
+    private void setMainView() {
+        mainView = (ListView)findViewById(R.id.mainView);
+        SimpleAdapter adapter = new SimpleAdapter(this, scList, R.layout.listview_layout,
+                new String[]{"Number", "scName"}, new int[]{R.id.number, R.id.scname});
+        mainView.setAdapter(adapter);
     }
 }

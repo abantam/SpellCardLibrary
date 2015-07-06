45aec99f1860e205340180a0f6c6868af1d9c36d
abantam <abantam.9@gmail.com>
2015-07-06 13:35:08 +0900

    FragmentAdapterを作成

---
+++ app/src/main/java/com/example/spellcardlibrary/app/MainFragmentAdapter.java
@@ -0,0 +1,34 @@
+package com.example.spellcardlibrary.app;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by admin on 2015/07/06.
+ */
+public class MainFragmentAdapter extends FragmentPagerAdapter {
+
+    public MainFragmentAdapter(android.support.v4.app.FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position) {
+            case 0:
+
+            default:
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "a";
+    }
+}

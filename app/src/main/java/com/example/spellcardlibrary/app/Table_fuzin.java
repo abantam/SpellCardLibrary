b600b00f52de9b9c2aad2898ab1b511b0de2d19a
abantam <abantam.9@gmail.com>
2015-10-07 01:56:14 +0900

    途中まで作品を書き終えた

---
+++ app/src/main/java/com/example/spellcardlibrary/app/Table_fuzin.java
@@ -0,0 +1,44 @@
+package com.example.spellcardlibrary.app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by admin on 2015/07/09.
+ */
+public class Table_fuzin extends BaseTable {
+
+    public Table_fuzin() {
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("“Œ•û•—_˜^");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public String getTitle() {return super.getTitle();}
+
+    @Override
+    public void setTitle(String title) {super.setTitle(title);}
+
+}
---
+++ app/src/main/java/com/example/spellcardlibrary/app/Table_tirei.java
@@ -0,0 +1,44 @@
+package com.example.spellcardlibrary.app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by admin on 2015/07/09.
+ */
+public class Table_tirei extends BaseTable {
+
+    public Table_tirei() {
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("“Œ•û’n—ì“a");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public String getTitle() {return super.getTitle();}
+
+    @Override
+    public void setTitle(String title) {super.setTitle(title);}
+
+}

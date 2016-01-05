34444752b1e3d553354ded0d449c6ec5c5e573ce
Takeshi Shimizu <abantam.9@gmail.com>
2016-01-05 22:57:24 +0900

    ListPagerAdapterをMainActivityのインナークラスにすることでgetItemメソッドのswitch文を削除

--- app/src/main/java/com/example/spellcardlibrary/app/MainActivity.java
+++ app/src/main/java/com/example/spellcardlibrary/app/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     private static final int credit_ID = 0;
 
     //ViewPagerを使用する際に使用するアダプター
-    private ListPagerAdapter2 pagerAdapter;
+    private ListPagerAdapter pagerAdapter;
 
     //スワイプでタブを切り替えるためのViewPager
     private ViewPager mViewPager;
@@ -32,9 +32,8 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
     private FragmentManager manager;
 
-    private BaseTable mFragment;
-
     private ActionBar actionBar;
+    private final int TAB_COUNT = 17;//タブの総数
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +44,11 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         manager = getSupportFragmentManager();
 
         //PagerAdapterを生成
-        mViewPager = (ViewPager)findViewById(R.id.pager);
-        //mViewPager.setOnPageChangeListener(this);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
         ListPagerAdapter adapter = new ListPagerAdapter(manager);
         mViewPager.setAdapter(adapter);
 
-        //ActスワイプしたときにもActionbarのタブ（NavigationItem）を常に表示させる処理
+        //スワイプしたときにもActionbarのタブ（NavigationItem）を常に表示させる処理
         mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
@@ -66,7 +64,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         //タブを生成
-        for(String title : titles) {
+        for (String title : titles) {
             makeTab(actionBar, manager, title);
         }
 
@@ -82,60 +80,22 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         tab.setTabListener(this);
         actionBar.addTab(tab);
 
-        //タブにフラグメントを登録
-//        Bundle bundle = new Bundle();
-//        bundle.putString("title", title);
-//        BaseTable fragment = new BaseTable();
-//        fragment.setArguments(bundle);
-//        fragment.setTitle(title);
-//        tab.setTag(fragment);
-//        manager.beginTransaction().add(R.id.parentLL, fragment, "title").commit();
+
     }
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-//        if(tab.getTag() == null) {
-//            String title = tab.getText().toString();
-//
-//            Bundle bundle = new Bundle();
-//            bundle.putString("title", title);
-//
-//            BaseTable fragment = new BaseTable();
-//            fragment.setArguments(bundle);
-//            fragment.setTitle(title);
-//            tab.setTag(fragment);
-//            manager.beginTransaction().add(R.id.parentLL, fragment, "title").commit();
-//        }else {
-
-            mViewPager.setCurrentItem(tab.getPosition());
-        //}
+        mViewPager.setCurrentItem(tab.getPosition());
     }
 
     @Override
     public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-//        BaseTable fragment = getFragmentFromTag(tab);
-//        if(fragment != null) {
-//            manager.beginTransaction().detach(fragment).commit();
-//        }
 
     }
 
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-//        BaseTable fragment = getFragmentFromTag(tab);
-//        manager.beginTransaction().attach(fragment).commit();
-    }
 
-    //タブにつけられたタグからフラグメントを取り出す
-    private BaseTable getFragmentFromTag(ActionBar.Tab tab) {
-        BaseTable fragment;
-        try {
-            fragment = (BaseTable)tab.getTag();
-        }catch(ClassCastException e) {
-            throw new ClassCastException();
-        }
-        return fragment;
     }
 
     //オプションメニューの作成
@@ -171,88 +131,41 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
                 return super.onOptionsItemSelected(item);
         }
     }
-}
 
-class ListPagerAdapter extends FragmentPagerAdapter {
 
-    public ListPagerAdapter(FragmentManager fm) {
-        super(fm);
-    }
+    //スワイプさせるためのPagerAdapterクラス
+    class ListPagerAdapter extends FragmentPagerAdapter {
 
-    @Override
-    public android.support.v4.app.Fragment getItem(int position) {
-
-        //作品名をタブの位置から判断しbundleに入れる
-        Bundle bundle = new Bundle();
-        switch(position) {
-            case 0:
-                bundle.putString("title", "東方紅魔郷");
-                break;
-            case 1:
-                bundle.putString("title", "東方妖々夢");
-                break;
-            case 2:
-                bundle.putString("title", "東方萃夢想");
-                break;
-            case 3:
-                bundle.putString("title", "東方永夜抄");
-                break;
-            case 4:
-                bundle.putString("title", "東方花映塚");
-                break;
-            case 5:
-                bundle.putString("title", "東方文花帖");
-                break;
-            case 6:
-                bundle.putString("title", "東方風神録");
-                break;
-            case 7:
-                bundle.putString("title", "東方緋想天");
-                break;
-            case 8:
-                bundle.putString("title", "東方地霊殿");
-                break;
-            case 9:
-                bundle.putString("title", "東方星蓮船");
-                break;
-            case 10:
-                bundle.putString("title", "東方非想天則");
-                break;
-            case 11:
-                bundle.putString("title", "ダブルスポイラー");
-                break;
-            case 12:
-                bundle.putString("title", "妖精大戦争");
-                break;
-            case 13:
-                bundle.putString("title", "東方神霊廟");
-                break;
-            case 14:
-                bundle.putString("title", "東方心綺楼");
-                break;
-            case 15:
-                bundle.putString("title", "東方輝針城");
-                break;
-            case 16:
-                bundle.putString("title", "弾幕アマノジャク");
-                break;
+        public ListPagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        //fragmentの生成
-        BaseTable fragment = new BaseTable();
-        fragment.setArguments(bundle);
-        return fragment;
-    }
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
 
-    @Override
-    public int getCount() {
-        return 17;
-    }
+            //作品名をタブの位置から判断しbundleに入れる
+            Bundle bundle = new Bundle();
+            bundle.putString("title", titles[position]);
 
-    @Override
-    public CharSequence getPageTitle(int position) {
-        return "touhoukoumakyou";
+            //fragmentの生成
+            BaseTable fragment = new BaseTable();
+            fragment.setArguments(bundle);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return TAB_COUNT;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
     }
 
 }
+
+
 

fca3454759cabeb961942a40b030232a45c0a372
Takeshi Shimizu <abantam.9@gmail.com>
2016-01-06 16:20:41 +0900

    不要なコードを削除

--- app/src/main/java/com/example/spellcardlibrary/app/BaseTable.java
+++ app/src/main/java/com/example/spellcardlibrary/app/BaseTable.java
@@ -22,38 +22,17 @@ import java.util.HashMap;
 
 public class BaseTable extends ListFragment {
 
-    private ListView mainView;//リストビュー
     private static SQLiteDatabase db;//データベースを格納
     private DatabaseHelper mDbHelper;
     private ArrayList<HashMap<String, String>> scList = new ArrayList<HashMap<String, String>>();
-    private ArrayList<String> scOnlyNameList = new ArrayList<String>();//外伝用スペルカードリスト
     private String title;//作品名
 
-//    public interface OnOkBtnClickListener {
-//        public void onOkClicked();
-//    }
-//
-//    private OnOkBtnClickListener mListener;
-
     public BaseTable() {}
-
-//    @Override
-//    public void onAttach(Activity activity) {
-//        super.onAttach(activity);
-//
-//        if(activity instanceof OnOkBtnClickListener == false) {
-//            throw new ClassCastException();
-//        }
-//
-//        mListener = ((OnOkBtnClickListener) activity);
-//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //scList = new ArrayList<HashMap<String, String>>();
         setDatabase();
-        Log.v("hoge", "hoge");
     }
 
     @Override
@@ -107,67 +86,6 @@ public class BaseTable extends ListFragment {
         }catch(SQLException sqle) {
             throw sqle;
         }
-    }
-
-    //コピーしたデータベースからリストを作成
-//    private void makeList() {
-//
-//        //インテントを取得したあと作品名を取得
-////        Intent intent = getIntent();
-////        workname = intent.getStringExtra("作品名");
-//
-//        //データベース内のカーソルを設定
-//        Cursor c = db.query(workname, null, null, null, null, null, null);
-//        c.moveToFirst();
-//
-//
-//
-//            //カーソルを1つずつ動かして一時変数にデータを格納
-//            for (int i = 0; i < c.getCount(); i++, c.moveToNext()) {
-//                HashMap<String, String> temp = new HashMap<String, String>();
-//                if(workname.equals("東方萃夢想") || workname.equals("東方花映塚") || workname.equals("東方緋想天") || workname.equals("東方非想天則") || workname.equals("東方心綺楼")) {
-//                    temp.put("Number", null);
-//                    temp.put("scName", c.getString(1));
-//                }else {
-//                    temp.put("Number", c.getString(1));
-//                    temp.put("scName", c.getString(2));
-//                }
-//
-//                scList.add(temp);
-//            }
-//
-//    }
-
-    //リストビューをセットしてリストを表示
-//    private void setMainView() {
-//        mainView = (ListView)findViewById(R.id.mainView);
-//        SimpleAdapter adapter = new SimpleAdapter(this, scList, R.layout.listview_layout,
-//                new String[]{"Number", "scName"}, new int[]{R.id.number, R.id.scname});
-//        mainView.setAdapter(adapter);
-//
-//        /*
-//        端末の画面サイズに合わせてフォントサイズを最適化する
-//        参考文献：http://cleanings.j
-//         */
-//        /*WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
-//        Display disp = wm.getDefaultDisplay();
-//        float width = disp.getWidth();
-//        float scale = width / 480;
-//        TextView text = (TextView)findViewById(R.id.)*/
-//
-//        //リストビューを選択したときに画面遷移
-//        mainView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent i = new Intent(BaseTable.this, SCInfo.class);
-//                i.putExtra("query", "SELECT * FROM " + workname + " WHERE _id = " + (position + 1));
-//                startActivity(i);
-//            }
-//        });
-//    }
-
-    //データベースを返す
-    public static SQLiteDatabase getDatabase() {
-        return db;
     }
 
     //スクロール制御
